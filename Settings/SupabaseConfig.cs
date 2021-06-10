16d41b0049a6210569976db5abe5eb3ed3404a40
Dan Rousseau <36530232+TheLazyLemur@users.noreply.github.com>
2021-06-10 09:34:37 +0200

    Update SupabaseConfig.cs

--- Settings/SupabaseConfig.cs
+++ Settings/SupabaseConfig.cs
@@ -2,7 +2,7 @@ namespace BlazorToDoList.Settings
 {
     public static class SupabaseConfig
     {
-        public static string Url = "https://hoooblvlddpueemxkoxv.supabase.co/";
-        public static string ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMjg4MjMyOCwiZXhwIjoxOTM4NDU4MzI4fQ.k5YAgmxJXpDigHaOVVGBxDuFIWIeb6bAc95DAnZPfts";
+        public static string Url = "supabase endpoint";
+        public static string ApiKey = "supabase key";
     }
 }
